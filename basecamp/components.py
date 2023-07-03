74f54e0aab817e3495fece1c8c7158f5e1ca7f24
Kaushik <50703773+thekaushikls@users.noreply.github.com>
2023-07-03 11:33:37 +0200

    [added] gh-action

--- basecamp/components.py
+++ basecamp/components.py
@@ -1,26 +1,9 @@
 from ghpythonlib.componentbase import dotnetcompiledcomponent as component
-import Rhino, Grasshopper, GhPython
+import Rhino, Grasshopper
 import System
 
 from elements import Tent
 from goo import TentGoo
-
-class AssemblyInfo(GhPython.Assemblies.PythonAssemblyInfo):
-    
-    def get_AssemblyName(self):
-        return "BaseCamp"
-    
-    def get_AssemblyDescription(self):
-        return """"""
-
-    def get_AssemblyVersion(self):
-        return "0.1"
-
-    def get_AuthorName(self):
-        return "Kaushik"
-    
-    def get_Id(self):
-        return System.Guid("27990269-51e7-467a-bc3e-b2b00b7391c4")
 
 class BaseCampComponent(component):
     
---
+++ basecamp/plugin.py
@@ -0,0 +1,18 @@
+import GhPython
+
+class AssemblyInfo(GhPython.Assemblies.PythonAssemblyInfo):
+    
+    def get_AssemblyName(self):
+        return "BaseCamp"
+    
+    def get_AssemblyDescription(self):
+        return """"""
+
+    def get_AssemblyVersion(self):
+        return "1.0.0"
+
+    def get_AuthorName(self):
+        return "Kaushik LS"
+    
+    def get_Id(self):
+        return System.Guid("27990269-51e7-467a-bc3e-b2b00b7391c4")
